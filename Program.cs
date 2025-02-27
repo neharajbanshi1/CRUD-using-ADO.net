56038e5847422f84f5333a338e53525e0f5a33c2
Neha Rajbanshi <neharajbanshi@Nehas-MacBook-Air.local>
2025-02-27 18:24:58 +0545

    Connecting C# to mysql

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Connecting c# with mysql
+using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "SERVER=localhost; DATABASE=employee_db; UID=csharpuser; PASSWORD=mypassword; AllowPublicKeyRetrieval=True; SSL Mode=None;";
+
+
+        try
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                Console.WriteLine("✅ Connected to MySQL database successfully!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("❌ Error: " + ex.Message);
+        }
+    }
+}
